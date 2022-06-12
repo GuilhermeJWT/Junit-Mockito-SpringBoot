548e6a7faecc0ee2a6ac01957551a2831d26c527
GuilhermeJWT <guiromanno@gmail.com>
2022-06-12 17:39:53 -0300

    Setup Inicial

---
+++ src/main/java/br/com/systemsgs/TestesJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesJunitMockitoApplication.class, args);
+	}
+
+}
