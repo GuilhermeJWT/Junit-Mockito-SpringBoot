8f339e353b362b9adb67c21bb93bd8c743f170bb
GuilhermeJWT <guiromanno@gmail.com>
2022-06-13 15:55:48 -0300

    Tratamento de Exception e Handler

---
+++ src/main/java/br/com/systemsgs/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.systemsgs.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+
+}
--- src/main/java/br/com/systemsgs/service/UserServiceImpl.java
+++ src/main/java/br/com/systemsgs/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.systemsgs.service;
 
 import br.com.systemsgs.domain.ModelUser;
+import br.com.systemsgs.exception.ObjectNotFoundException;
 import br.com.systemsgs.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class UserServiceImpl implements UserService{
     @Override
     public ModelUser findById(Integer id) {
         Optional<ModelUser> user = repository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado!"));
     }
 
 }
