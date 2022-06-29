985efdf2d49ecab7800537742a325fbfff28ec92
GuilhermeJWT <guiromanno@gmail.com>
2022-06-29 10:51:20 -0300

    Teste - create sucess

--- src/test/java/br/com/systemsgs/service/UserServiceImplTest.java
+++ src/test/java/br/com/systemsgs/service/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -92,7 +93,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess() {
+        when(repository.save(any())).thenReturn(user);
+
+        ModelUser response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(ModelUser.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
